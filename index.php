161807fc6f62111294959bdca219db4845627f1f
Saitamchik <48436223+Saitamchik@users.noreply.github.com>
2019-04-02 23:16:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <html>
 <head>
-	<title>Главная страница - страница обо мне</title>
 </head>
 <body>
 <center><h2>MLG Blog</h2></center>
 <br/><br/>
-</center>
 <br/><br/>
    <?php
-//    $mass = "Mass.json";
-//    $content = file_get_contents($mass);
-//   $content = json_decode($content,true);
-//    print_r($content);
+    $mass = "Mass.json";
+    $content = file_get_contents($mass);
+   $content = json_decode($content,true);
     $massiv = array(
     1 => "Post_1",
     2 => "Post_2",
@@ -23,5 +20,7 @@ echo($massiv[$id]);
 $id++;
 };
     ?>
+<br/><br/>
+<center> <a href="https://github.com/Saitamchik/Blog">Репозиторий</a></center>
 </body>
 </html>
